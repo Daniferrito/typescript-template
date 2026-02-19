743fd66423b19d1e70f2b684067db988e3652aad
dferri <dferri@dawe.gg>
2026-02-19 12:57:32 +0100

    feat: dont join useless factions

--- src/utils/factionHandling.ts
+++ src/utils/factionHandling.ts
@@ -23,11 +23,24 @@ export async function joinFactions(ns: NS): Promise<boolean> {
   }
   const invitations = ns.singularity.checkFactionInvitations()
   for (const faction of invitations) {
-    if (ns.singularity.getFactionEnemies(faction).length === 0) {
+    // Only join factions that dont have enemies and that have augmentations we don't have yet, to avoid joining factions that we will never do anything with
+    if (ns.singularity.getFactionEnemies(faction).length === 0 && hasRemainingAugmentations(ns, faction)) {
       if (ns.singularity.joinFaction(faction)) {
         hadChanges = true;
       }
     }
   }
   return hadChanges;
+}
+
+function hasRemainingAugmentations(ns: NS, faction: string): boolean {
+  const augmentations = ns.singularity.getAugmentationsFromFaction(faction)
+  const ownedAugmentations = ns.singularity.getOwnedAugmentations(true)
+  return augmentations.some(a => !ownedAugmentations.includes(a) || otherJoinedFactionHasAugmentation(ns, a))
+}
+
+function otherJoinedFactionHasAugmentation(ns: NS, augmentation: string): boolean {
+  const factions = ns.singularity.getAugmentationFactions(augmentation)
+  const player = ns.getPlayer()
+  return factions.some(f => player.factions.includes(f))
 }
